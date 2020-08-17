4aa67aca81c185766d2af055dd1e3e3f1c9c10c2
Steve Min <stevekmin@gmail.com>
2020-08-17 10:37:01 -0700

    add basic UI table imort useEffect and useState for future use

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 
 const ListTodos = () => {
-  return <h1>List Todos</h1>;
+  return (
+    <Fragment>
+      <table className='table mt-5 text-center'>
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* <tr>
+            <td>John</td>
+            <td>Doe</td>
+            <td>john@example.com</td>
+          </tr>  */}
+        </tbody>
+      </table>
+    </Fragment>
+  );
 };
 
 export default ListTodos;
