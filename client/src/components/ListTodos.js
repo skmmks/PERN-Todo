4acc235885af9a2be96d8ceb06895575a0a6c2be
Steve Min <stevekmin@gmail.com>
2020-08-24 12:54:13 -0700

    add delete todo skeleton function add delete function to click button

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -3,6 +3,17 @@ import React, { Fragment, useEffect, useState } from 'react';
 const ListTodos = () => {
   const [todos, setTodos] = useState([]);
 
+  const deleteTodo = async (id) => {
+    try {
+      const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+        method: 'DELETE',
+      });
+      console.log(deleteTodo);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
   const getTodos = async () => {
     try {
       const response = await fetch('http://localhost:5000/todos');
@@ -41,7 +52,9 @@ const ListTodos = () => {
               <td>{todo.description}</td>
               <td>Edit</td>
               <td>
-                <button className='btn btn-danger'>Delete</button>
+                <button className='btn btn-danger' onClick={() => deleteTodo(todo.todo_id)}>
+                  Delete
+                </button>
               </td>
             </tr>
           ))}
