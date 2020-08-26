513ac0f4fbc2cf46415d02f9d88bbbd8f668ec12
Steve Min <stevekmin@gmail.com>
2020-08-26 13:19:50 -0700

    add fix to localhost PUT method

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -7,11 +7,12 @@ const EditTodo = ({ todo }) => {
     e.preventDefault();
     try {
       const body = { description };
-      const response = await fetch(`http//localhost:5000/todos/${todo.todo_id}`, {
+      const response = await fetch(`http://localhost:5000/todos/${todo.todo_id}`, {
         method: 'PUT',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(body),
       });
+      console.log(response);
     } catch (error) {
       console.error(error.message);
     }
