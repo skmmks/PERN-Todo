56201322ff9538f425b0256f7d7878a16bca7c39
Steve Min <stevekmin@gmail.com>
2020-08-26 13:18:12 -0700

    add function to edit todo item add function to click handler

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -2,13 +2,27 @@ import React, { Fragment, useState } from 'react';
 
 const EditTodo = ({ todo }) => {
   const [description, setDescription] = useState(todo.description);
+
+  const updateDescription = async (e) => {
+    e.preventDefault();
+    try {
+      const body = { description };
+      const response = await fetch(`http//localhost:5000/todos/${todo.todo_id}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body),
+      });
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
   return (
     <Fragment>
-      <button type='button' className='btn btn-warning' data-toggle='modal' data-target='#myModal'>
+      <button type='button' className='btn btn-warning' data-toggle='modal' data-target={`#id${todo.todo_id}`}>
         Edit
       </button>
 
-      <div className='modal' id='myModal'>
+      <div className='modal' id={`id${todo.todo_id}`}>
         <div className='modal-dialog'>
           <div className='modal-content'>
             <div className='modal-header'>
@@ -19,11 +33,21 @@ const EditTodo = ({ todo }) => {
             </div>
 
             <div className='modal-body'>
-              <input type='text' className='form-control' />
+              <input
+                type='text'
+                className='form-control'
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              />
             </div>
 
             <div className='modal-footer'>
-              <button type='button' className='btn btn-warning' data-dismiss='modal'>
+              <button
+                type='button'
+                className='btn btn-warning'
+                data-dismiss='modal'
+                onClick={(e) => updateDescription(e)}
+              >
                 Edit
               </button>
               <button type='button' className='btn btn-danger' data-dismiss='modal'>
